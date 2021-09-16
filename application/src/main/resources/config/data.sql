0b2722d15a65b2755d1128b2474c42fb2442d0f5
GermanoGiudici <germano.giudici@gmail.com>
2021-09-16 10:06:00 +0200

    inserts

--- application/src/main/resources/config/data.sql
+++ application/src/main/resources/config/data.sql
@@ -1,9 +1,6 @@
-INSERT INTO PUBLIC.CATEGORY (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'category1Description', 'category1Name');
-INSERT INTO PUBLIC.CATEGORY (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'category2Description', 'category2Name');
-INSERT INTO PUBLIC.CATEGORY (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'category3Description', 'category3Name');
-INSERT INTO PUBLIC.CATEGORY (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'category4Description', 'category4Name');
-INSERT INTO PUBLIC.CATEGORY (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'category5Description', 'category5Name');
-INSERT INTO PUBLIC.CATEGORY (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'category6Description', 'category6Name');
+INSERT INTO PUBLIC.CATEGORY (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'Component Collection', 'Component Collection');
+INSERT INTO PUBLIC.CATEGORY (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'PBC', 'PBC');
+INSERT INTO PUBLIC.CATEGORY (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'Solution Template', 'Solution Template');
 
 insert into ORGANISATION (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'organisation1Description', 'organisation1Name');
 insert into ORGANISATION (ID, DESCRIPTION, NAME) VALUES (nextval('HIBERNATE_SEQUENCE'), 'organisation2Description', 'organisation2Name');

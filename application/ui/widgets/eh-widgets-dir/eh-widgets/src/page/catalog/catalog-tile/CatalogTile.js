2a74dfe6bf6eacd9fd2fdcfa80b3b7608a378780
davdet <dv.dt@hotmail.com>
2021-09-17 14:57:49 +0200

    Labels active

--- application/ui/widgets/eh-widgets-dir/eh-widgets/src/page/catalog/catalog-tile/CatalogTile.js
+++ application/ui/widgets/eh-widgets-dir/eh-widgets/src/page/catalog/catalog-tile/CatalogTile.js
@@ -1,44 +1,54 @@
-import React, {useEffect, useState} from 'react';
-import {ClickableTile, Tag} from "carbon-components-react";
-import {useHistory} from "react-router-dom";
-import {getSingleCategory} from "../../../integration/Integration";
+import React, { useEffect, useState } from "react";
+import { ClickableTile, Tag } from "carbon-components-react";
+import { useHistory } from "react-router-dom";
+import { getSingleCategory } from "../../../integration/Integration";
 
-import "./catalog-tile.scss"
+import "./catalog-tile.scss";
 
 const CatalogTile = (CatalogTileProps) => {
-    const {bundleGroupId, name, description, categories, status} = CatalogTileProps
-    const [categoryName,setCategoryName] = useState("")
-    useEffect(()=>{
-        (async ()=>{
-            const data = await getSingleCategory(categories[0])
-            setCategoryName(data.category.name)
-        })(categories)
-    })
+  const { bundleGroupId, name, description, categories, status } =
+    CatalogTileProps;
+  const [categoryName, setCategoryName] = useState("");
+  useEffect(() => {
+    (async () => {
+      const data = await getSingleCategory(categories[0]);
+      setCategoryName(data.category.name);
+    })(categories);
+  });
 
-    const history = useHistory()
+  const history = useHistory();
 
-    const handleClick = (evt) => {
-        history.push("/bundlegroup/" + bundleGroupId)
-    }
+  const handleClick = (evt) => {
+    history.push("/bundlegroup/" + bundleGroupId);
+  };
 
-  return <ClickableTile handleClick={handleClick} className="CatalogTile">
-    <div className="CatalogTile-card-wrapper">
-      <div className="CatalogTile-card-icon">
-        <img src={`${process.env.REACT_APP_PUBLIC_ASSETS_URL}/icon.svg`} alt="Entando logo"/>
+  const statusToRender = status === "PUBLISHED" ? 'Published' : 'Unpublished';
+
+  return (
+    <ClickableTile handleClick={handleClick} className="CatalogTile">
+      <div className="CatalogTile-card-wrapper">
+        <div className="CatalogTile-card-icon">
+          <img
+            src={`${process.env.REACT_APP_PUBLIC_ASSETS_URL}/icon.svg`}
+            alt="Entando logo"
+          />
+        </div>
+        <div className="CatalogTile-card-title">{name}</div>
+        <div className="CatalogTile-card-status">{statusToRender}</div>
+        <div className="CatalogTile-card-description">
+          {description}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <div className="CatalogTile-card-category">
+          {/*tag list*/}
+          <Tag type="blue" title="Clear Filter">
+            {" "}
+            {categoryName}{" "}
+          </Tag>
+        </div>
       </div>
-      <div className="CatalogTile-card-title">
-        {name}
-      </div>
-      <div className="CatalogTile-card-description">
-        {description} - {status}
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </div>
-      <div className="CatalogTile-card-category">
-        {/*tag list*/}
-        <Tag type="blue" title="Clear Filter"> {categoryName} </Tag>
-      </div>
-    </div>
-  </ClickableTile>
-}
+    </ClickableTile>
+  );
+};
 
 export default CatalogTile;

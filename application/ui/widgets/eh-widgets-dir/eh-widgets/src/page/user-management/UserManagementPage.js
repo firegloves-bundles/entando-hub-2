32e98784556198c42d93f84cc05283948d3df349
DMinnai <d.minnai@entando.com>
2021-10-07 18:09:16 +0200

    user managment list

--- application/ui/widgets/eh-widgets-dir/eh-widgets/src/page/user-management/UserManagementPage.js
+++ application/ui/widgets/eh-widgets-dir/eh-widgets/src/page/user-management/UserManagementPage.js
@@ -1,24 +1,24 @@
 import React, {useEffect, useState} from "react"
 
-
 import {
-    Content,
-    DataTable,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableHeader,
-    TableRow,
-    TableToolbar,
-    TableToolbarContent,
+  Content,
+  DataTable,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableToolbar,
+  TableToolbarContent,
 } from 'carbon-components-react'
-import UserManagementOverflowMenu from "./overflow-menu/UserManagementOverflowMenu"
+import UserManagementOverflowMenu
+  from "./overflow-menu/UserManagementOverflowMenu"
 import {ModalAddNewUser} from "./modal-add-new-user/ModalAddNewUser"
 import {getAllUsers, getSingleOrganisation} from "../../integration/Integration"
 import EhBreadcrumb from "../../components/eh-bradcrumb/EhBreadcrumb";
-
+import "./user-managment-page.scss"
 
 /*
 BUNDLEGROUP:
@@ -49,129 +49,143 @@ bundleId	string
  */
 
 const headers = [
-    {
-        key: 'username',
-        header: 'Username',
-    },
-    {
-        key: 'email',
-        header: 'Email',
-    },
-    {
-        key: 'organisation',
-        header: 'Organisation',
-    },
-    {
-        key: 'overflow',
-        header: '',
-    }
+  {
+    key: 'username',
+    header: 'Username',
+  },
+  {
+    key: 'email',
+    header: 'Email',
+  },
+  {
+    key: 'organisation',
+    header: 'Organisation',
+  },
+  {
+    key: 'overflow',
+    header: '',
+  }
 ]
 
-
 const UserManagementPage = () => {
-    const [reloadToken, setReloadToken] = useState(((new Date()).getTime()).toString())
-    const [users, setUsers] = useState([])
+  const [reloadToken, setReloadToken] = useState(
+      ((new Date()).getTime()).toString())
+  const [users, setUsers] = useState([])
 
+  // fetches the users to show
+  useEffect(() => {
 
+    //TODO BE QUERY REFACTORING
+    const getAllPortalUsers = async () => {
+      const userList = (await getAllUsers()).userList
+      //for every user get the organisations name
+      const userListWithOrganisation = await Promise.all(
+          userList.map((async (user) => {
+            if (user.organisationIds) {
+              //get the current organisation name
+              const organisations = await Promise.all(
+                  user.organisationIds.map((async (oid) => {
+                        const organisation = (await getSingleOrganisation(
+                            oid)).organisation
+                        return organisation
+                      }
+                  )))
 
-    // fetches the users to show
-    useEffect(() => {
+              return {
+                ...user,
+                email: user.email ? user.email : "",
+                organisation: organisations[0]
+              }
+            }
+            return {
+              ...user,
+              email: user.email ? user.email : "",
+              organisation: null
+            }
+          })))
 
-        //TODO BE QUERY REFACTORING
-        const getAllPortalUsers = async ()=>{
-            const userList = (await getAllUsers()).userList
-            //for every user get the organisations name
-            const userListWithOrganisation = await Promise.all(userList.map((async (user) => {
-                if (user.organisationIds) {
-                    //get the current organisation name
-                    const organisations = await Promise.all(user.organisationIds.map((async (oid) => {
-                            const organisation = (await getSingleOrganisation(oid)).organisation
-                            return organisation
-                        }
-                    )))
+      return userListWithOrganisation
+    };
+    (async () => {
+      setUsers(await getAllPortalUsers())
+    })()
+  }, [reloadToken])
 
-                    return {
-                        ...user,
-                        email: user.email?user.email:"",
-                        organisation: organisations[0]
-                    }
-                }
-                return {
-                    ...user,
-                    email: user.email?user.email:"",
-                    organisation: null
-                }
-            })))
+  const onAfterSubmit = () => {
+    setReloadToken(((new Date()).getTime()).toString())
+  }
+  return (
+      <>
+        <Content className="UserManagementPage">
+          <div
+              className="UserManagementPage-wrapper">
+            <div className="bx--row">
+              <div className="bx--col-lg-16 UserManagementPage-breadcrumb">
+                <EhBreadcrumb/>
+              </div>
+            </div>
+            <div className="bx--row">
+              <div className="bx--col-lg-16 UserManagementPage-section">
+                <DataTable rows={users} headers={headers}>
+                  {({
+                    rows,
+                    headers,
+                    getTableProps,
+                    getHeaderProps,
+                    getRowProps
+                  }) => (
+                      <TableContainer title="Users Management">
+                        <TableToolbar>
+                          <TableToolbarContent>
+                            <ModalAddNewUser onAfterSubmit={onAfterSubmit}/>
+                          </TableToolbarContent>
+                        </TableToolbar>
+                        <Table {...getTableProps()}>
+                          <TableHead>
+                            <TableRow>
+                              {headers.map((header) => (
+                                  <TableHeader {...getHeaderProps({header})}>
+                                    {header.header}
+                                  </TableHeader>
+                              ))}
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                            {rows.map(row => (
+                                <TableRow {...getRowProps({row})}>
+                                  {row.cells.map((cell, index) => {
+                                        if (cell.id !== row.id
+                                            + ":overflow") {
+                                          return <TableCell
+                                              key={cell.id}>{index === 2
+                                              ? cell.value
+                                                  ? cell.value.name : "---"
+                                              : cell.value}</TableCell>
+                                        }
+                                        return <TableCell
+                                            key={cell.id}><UserManagementOverflowMenu
+                                            userObj={{
+                                              username: row.cells[0].value,
+                                              email: row.cells[1].value,
+                                              organisation: row.cells[2].value
+                                            }}
+                                            onAfterSubmit={onAfterSubmit}/></TableCell>
+                                      }
+                                  )}
+                                </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      </TableContainer>
+                  )}
+                </DataTable>
+              </div>
+            </div>
+          </div>
+        </Content>
+      </>
 
-            return userListWithOrganisation
-        };
-        (async () => {
-            setUsers(await getAllPortalUsers())
-        })()
-    }, [reloadToken])
-
-    const onAfterSubmit = () => {
-        setReloadToken(((new Date()).getTime()).toString())
-    }
-    return (
-        <>
-            <Content className="CatalogPage">
-                <div className="bx--grid bx--grid--full-width catalog-page">
-                    <div className="bx--row">
-                        <div className="bx--col-lg-16 CatalogPage-breadcrumb">
-                            <EhBreadcrumb/>
-                        </div>
-                    </div>
-                    <div className="bx--row">
-                        <div className="bx--col-lg-16 CatalogPage-section">
-                            <DataTable rows={users} headers={headers}>
-                                {({rows, headers, getTableProps, getHeaderProps, getRowProps}) => (
-                                    <TableContainer title="Users Management">
-                                        <TableToolbar>
-                                            <TableToolbarContent>
-                                                <ModalAddNewUser onAfterSubmit={onAfterSubmit}/>
-                                            </TableToolbarContent>
-                                        </TableToolbar>
-                                        <Table {...getTableProps()}>
-                                            <TableHead>
-                                                <TableRow>
-                                                    {headers.map((header) => (
-                                                        <TableHeader {...getHeaderProps({header})}>
-                                                            {header.header}
-                                                        </TableHeader>
-                                                    ))}
-                                                </TableRow>
-                                            </TableHead>
-                                            <TableBody>
-                                                {rows.map(row => (
-                                                    <TableRow {...getRowProps({row})}>
-                                                        {row.cells.map((cell, index) => {
-                                                                if (cell.id !== row.id + ":overflow") return <TableCell
-                                                                    key={cell.id}>{index === 2 ? cell.value ? cell.value.name : "---" : cell.value}</TableCell>
-                                                                return <TableCell key={cell.id}><UserManagementOverflowMenu
-                                                                    userObj={{
-                                                                        username: row.cells[0].value,
-                                                                        email: row.cells[1].value,
-                                                                        organisation: row.cells[2].value
-                                                                    }} onAfterSubmit={onAfterSubmit}/></TableCell>
-                                                            }
-                                                        )}
-                                                    </TableRow>
-                                                ))}
-                                            </TableBody>
-                                        </Table>
-                                    </TableContainer>
-                                )}
-                            </DataTable>
-                        </div>
-                    </div>
-                </div>
-            </Content>
-        </>
-
-
-    )
+  )
 }
-
 
 export default UserManagementPage

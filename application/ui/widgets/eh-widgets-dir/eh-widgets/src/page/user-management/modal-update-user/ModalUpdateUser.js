695b7377ef1f15a08ee55be88093d7dd97577675
davdet <dv.dt@hotmail.com>
2021-10-14 11:27:46 +0200

    Adds loader

--- application/ui/widgets/eh-widgets-dir/eh-widgets/src/page/user-management/modal-update-user/ModalUpdateUser.js
+++ application/ui/widgets/eh-widgets-dir/eh-widgets/src/page/user-management/modal-update-user/ModalUpdateUser.js
@@ -1,58 +1,66 @@
-import {Modal} from "carbon-components-react"
-import {useCallback, useState} from "react"
+import { Loading, Modal } from "carbon-components-react"
+import { useCallback, useState } from "react"
 import UpdateUser from "./update-user/UpdateUser"
 import {
-    createAUserForAnOrganisation,
-    getAllOrganisations,
-    removeUserFromOrganisation
+  createAUserForAnOrganisation,
+  getAllOrganisations,
+  removeUserFromOrganisation,
 } from "../../../integration/Integration"
 
+export const ModalUpdateUser = ({
+  userObj,
+  open,
+  onCloseModal,
+  onAfterSubmit,
+}) => {
+  const [user, setUser] = useState(userObj)
+  const [visible, setVisible] = useState(false)
 
-export const ModalUpdateUser = ({userObj, open, onCloseModal, onAfterSubmit}) => {
+  const onDataChange = useCallback((userObj) => {
+    setUser(userObj)
+  }, [])
 
-    const [user, setUser] = useState(userObj)
+  const onRequestClose = (e) => {
+    onCloseModal()
+  }
 
-    const onDataChange = useCallback((userObj) => {
-        setUser(userObj)
-    }, [])
-
-    const onRequestClose = (e) => {
-        onCloseModal()
-    }
-
-
-    //TODO BE QUERY REFACTORING
-    const updateUser = async (user) => {
-        //delete all the organisations for the user
-        await Promise.all((await getAllOrganisations()).organisationList.map(async (oId) => {
-                await removeUserFromOrganisation(oId.organisationId, user.username)
-            }
-        ))
-
-        let organisationId = user.organisation.organisationId
-        await createAUserForAnOrganisation(organisationId, user.username)
-
-    }
-
-
-    const onRequestSubmit = (e) => {
-        (async () => {
-            await updateUser(user)
-            onCloseModal()
-            onAfterSubmit()
-
-        })()
-    }
-
-    return (
-        <Modal
-            modalLabel="Edit"
-            primaryButtonText="Save"
-            secondaryButtonText="Cancel"
-            open={open}
-            onRequestClose={onRequestClose}
-            onRequestSubmit={onRequestSubmit}>
-            <UpdateUser userObj={user} onDataChange={onDataChange}/>
-        </Modal>
+  //TODO BE QUERY REFACTORING
+  const updateUser = async (user) => {
+    //delete all the organisations for the user
+    await Promise.all(
+      (
+        await getAllOrganisations()
+      ).organisationList.map(async (oId) => {
+        await removeUserFromOrganisation(oId.organisationId, user.username)
+      })
     )
+
+    let organisationId = user.organisation.organisationId
+    await createAUserForAnOrganisation(organisationId, user.username)
+    setVisible(true)
+  }
+
+  const onRequestSubmit = (e) => {
+    ;(async () => {
+      await updateUser(user)
+      onCloseModal()
+      onAfterSubmit()
+    })()
+  }
+
+  return (
+    <>
+      {!visible && <Loading />}
+      <Modal
+        modalLabel="Edit"
+        primaryButtonText="Save"
+        secondaryButtonText="Cancel"
+        open={open}
+        onRequestClose={onRequestClose}
+        onRequestSubmit={onRequestSubmit}
+      >
+        <UpdateUser userObj={user} onDataChange={onDataChange} />
+      </Modal>
+    </>
+  )
 }

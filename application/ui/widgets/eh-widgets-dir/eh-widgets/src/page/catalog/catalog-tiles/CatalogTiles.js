05704bfe9781ef80f42008ec64c7b58493663229
Nathan Shaw <n.shaw@entando.com>
2021-11-06 23:33:22 -0400

    EHUB-101 Add guard on empty list of groups

--- application/ui/widgets/eh-widgets-dir/eh-widgets/src/page/catalog/catalog-tiles/CatalogTiles.js
+++ application/ui/widgets/eh-widgets-dir/eh-widgets/src/page/catalog/catalog-tiles/CatalogTiles.js
@@ -5,7 +5,7 @@ import CatalogTile from "../catalog-tile/CatalogTile"
  * renders a list of bundleGroup
  */
 const CatalogTiles = ({bundleGroups,onAfterSubmit}) => {
-    const listItems = bundleGroups.map((bundleGroup) => <CatalogTile onAfterSubmit={onAfterSubmit} descriptionImage={bundleGroup.descriptionImage} key={bundleGroup.bundleGroupId} {...bundleGroup}/>)
+    const listItems = bundleGroups && bundleGroups.map((bundleGroup) => <CatalogTile onAfterSubmit={onAfterSubmit} descriptionImage={bundleGroup.descriptionImage} key={bundleGroup.bundleGroupId} {...bundleGroup}/>)
     return <div>{listItems}</div>
 }
 

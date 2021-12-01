4b601a40ce1a5280e84636002b00d4d6c80f57a7
Kamlesh Bobde <kamlesh.bobde@newvisionsoftware.in>
2021-12-01 12:13:43 +0530

    EHUB-121: Added Remove option option in dropdown to remove a user from an organization. One change in RemoveUserFromOrg api.

--- application/ui/widgets/eh-widgets-dir/eh-widgets/src/helpers/constants.js
+++ application/ui/widgets/eh-widgets-dir/eh-widgets/src/helpers/constants.js
@@ -1,4 +1,3 @@
-
 //Hub Roles
 export const AUTHOR = 'eh-author'
 export const MANAGER = 'eh-manager'
@@ -17,18 +16,35 @@ export const BUTTON_LABELS = {
     DELETE: "Delete",
     EDIT: "Edit",
     CANCEL: "Cancel",
+    REMOVE: "Remove",
 }
 
 // All Modal Labels
 export const MODAL_LABELS = {
     DELETE_BUNDLE_MSG: "Are you sure you want to delete this bundle?",
+    REMOVE_USER_FROM_ORG_MSG: "Are you sure, you want to remove this User from the Organization?",
 }
 
 // All API Response Key
 export const API_RESPONSE_KEY = {
-    EDITED_BUNDLE_GROUP : 'editedBundleGroup'
+    EDITED_BUNDLE_GROUP: 'editedBundleGroup'
 }
 
 // Constant String
 export const DELETED_BUNDLE = 'deletedBundle';
 export const GIT_REPO = 'gitRepo';
+
+/**
+ * Messages
+ */
+export const MESSAGES = {
+    NOTIFY_GUEST_PORTAL_USER_MSG: 'Your account does not currently have access to the Hub. Please contact your Administrator to request access.',
+    IMPOSSIBLE_TO_REMOVE_USERS_MSG: 'Impossible to remove the user',
+    USER_REMOVED_FROM_ORG_MSG: 'User removed from the organisation'
+}
+
+// All dropdown options
+export const DROPDOWN_OPTIONS = {
+    EDIT: "Edit",
+    REMOVE: "Remove",
+}

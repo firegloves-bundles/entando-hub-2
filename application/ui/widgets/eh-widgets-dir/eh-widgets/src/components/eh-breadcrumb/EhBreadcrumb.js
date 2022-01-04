98cfe185f253d9aedc280ccf388bf5baf6a21e08
Rohit Sengar <rohit.sengar@newvisionsoftware.in>
2022-01-04 12:09:41 +0530

    EHUB-136, Added Amin access .

--- application/ui/widgets/eh-widgets-dir/eh-widgets/src/components/eh-breadcrumb/EhBreadcrumb.js
+++ application/ui/widgets/eh-widgets-dir/eh-widgets/src/components/eh-breadcrumb/EhBreadcrumb.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {Breadcrumb, BreadcrumbItem} from "carbon-components-react"
-import {Link} from "react-router-dom"
+import {Link, useHistory} from "react-router-dom"
 import './eh-breadcrumb.scss'
 import i18n from '../../i18n'
+import { ADMIN } from '../../helpers/constants'
+import { getHigherRole } from '../../helpers/helpers'
 /*
 {
 path,
@@ -10,8 +12,13 @@ href
 }
  */
 const EhBreadcrumb = ({pathElements = []}) => {
+    const currentUrl = useHistory().location.pathname.split("/")[1]
+    const linkStyle = {
+        margin: "4px",
+        textDecoration: "none",
+      };
 
-    const elementList = pathElements.map((pathElement, index) => {
+    let elementList = pathElements.map((pathElement, index) => {
         if (index === pathElements.length - 1) {
             return (
                 <span key={index.toString()}>{pathElement.path}</span>
@@ -22,14 +29,37 @@ const EhBreadcrumb = ({pathElements = []}) => {
         </BreadcrumbItem>)
     })
 
+    if (!elementList.length) {
+      if (currentUrl === "admin")
+        elementList = <span className="navigation-breadcrumb">User</span>;
+      else if (currentUrl === "category")
+        elementList = <span className="navigation-breadcrumb">Category</span>;
+      else if (currentUrl === "organisation")
+        elementList = (
+          <span className="navigation-breadcrumb">Organisation</span>
+        );
+    }
     return (
-        <Breadcrumb aria-label="Page navigation">
-            <BreadcrumbItem>
-                <Link to="/">{i18n.t('page.catlogPanel.catlogHomePage.home')}</Link>
-            </BreadcrumbItem>
-            {elementList}
-        </Breadcrumb>
-    )
+      <Breadcrumb aria-label="Page navigation">
+        <BreadcrumbItem>
+          <Link to="/">{i18n.t("page.catlogPanel.catlogHomePage.home")}</Link>
+        </BreadcrumbItem>
+        {elementList}
+        {getHigherRole() === ADMIN && (
+          <div className="navigation-bar">
+            <Link style={linkStyle} to="/admin">
+              User Management
+            </Link>
+            <Link style={linkStyle} to="/category">
+              Category Management
+            </Link>
+            <Link style={linkStyle} to="/organisation">
+              Organisation Management
+            </Link>
+          </div>
+        )}
+      </Breadcrumb>
+    );
 }
 
 export default EhBreadcrumb

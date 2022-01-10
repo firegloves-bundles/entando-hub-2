43639bd3863823d230f36a6b960c858290538b77
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2022-01-10 11:01:39 +0530

    EHUB-136 removed inlined css.

--- application/ui/widgets/eh-widgets-dir/eh-widgets/src/components/eh-breadcrumb/EhBreadcrumb.js
+++ application/ui/widgets/eh-widgets-dir/eh-widgets/src/components/eh-breadcrumb/EhBreadcrumb.js
@@ -15,48 +15,41 @@ const EhBreadcrumb = ({pathElements = []}) => {
   const currentPath = (pathElements && pathElements.length && pathElements[0].page) ? pathElements[0].page : '';
   const showNavBar = Object.values(SHOW_NAVBAR_ON_MOUNTED_PAGE).includes(currentPath) && getHigherRole() === ADMIN;
 
-    const linkStyle = {
-        margin: "0px 4px",
-        textDecoration: "none",
-      };
+  let elementList = pathElements.map((pathElement, index) => {
+    if (index === pathElements.length - 1) {
+      return (
+        <span key={index.toString()}>{pathElement.path}</span>
+      )
+    }
+    return (<BreadcrumbItem key={index.toString()}>
+      <Link to={pathElement.href}>{pathElement.path}</Link>
+    </BreadcrumbItem>)
+  })
 
-    let elementList = pathElements.map((pathElement, index) => {
-        if (index === pathElements.length - 1) {
-            return (
-                <span key={index.toString()}>{pathElement.path}</span>
-            )
-        }
-        return (<BreadcrumbItem key={index.toString()}>
-            <Link to={pathElement.href}>{pathElement.path}</Link>
-        </BreadcrumbItem>)
-    })
-
-    return (
-      <Breadcrumb aria-label="Page navigation">
-        <BreadcrumbItem>
-          <Link to="/">{i18n.t("page.catlogPanel.catlogHomePage.home")}</Link>
-        </BreadcrumbItem>
-        {elementList}
-        {showNavBar && (
-          <div
-            className="navigation-bar"
-            style={currentPath === SHOW_NAVBAR_ON_MOUNTED_PAGE.isCatalogPage ? { "marginRight": "0rem" } : { "marginRight": "1.8rem" }}
-          >
-            <Link style={linkStyle} to="/admin">
-              {i18n.t('navLink.userManagement')}
-            </Link>
-            <pre> | </pre>
-            <Link style={linkStyle} to="/category">
-              {i18n.t('navLink.categoryManagement')}
-            </Link>
-            <pre> | </pre>
-            <Link style={linkStyle} to="/organisation">
-              {i18n.t('navLink.organisationManagement')}
-            </Link>
-          </div>
-        )}
-      </Breadcrumb>
-    );
+  return (
+    <Breadcrumb aria-label="Page navigation">
+      <BreadcrumbItem>
+        <Link to="/">{i18n.t("page.catlogPanel.catlogHomePage.home")}</Link>
+      </BreadcrumbItem>
+      {elementList}
+      {showNavBar && (
+        <div className="navigation-bar"
+          style={currentPath === SHOW_NAVBAR_ON_MOUNTED_PAGE.isCatalogPage ? { "marginRight": "0rem" } : { "marginRight": "1.8rem" }}>
+          <Link to="/admin">
+            {i18n.t('navLink.userManagement')}
+          </Link>
+          <pre> | </pre>
+          <Link to="/category">
+            {i18n.t('navLink.categoryManagement')}
+          </Link>
+          <pre> | </pre>
+          <Link to="/organisation">
+            {i18n.t('navLink.organisationManagement')}
+          </Link>
+        </div>
+      )}
+    </Breadcrumb>
+  );
 }
 
 export default EhBreadcrumb

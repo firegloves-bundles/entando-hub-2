9fccd536b82c46da357d0d85605554159210529a
Rohit Sengar <rohit.sengar@newvisionsoftware.in>
2022-01-10 14:37:02 +0530

    EHUB-180: Added CrossOrigin Configurations.

---
+++ application/src/main/java/com/entando/hub/catalog/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.entando.hub.catalog.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${ALLOWED_ORIGIN}")
+    private String allowedOrigin;
+
+    @Bean
+    public WebMvcConfigurer getCorsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigin)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}

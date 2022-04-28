826c63bdba686410b72a7ab653db12b84ed4ba63
GermanoGiudici <germano.giudici@gmail.com>
2022-04-28 12:10:38 +0200

    new rest api

--- application/src/main/java/com/entando/hub/catalog/rest/EntTemplateController.java
+++ application/src/main/java/com/entando/hub/catalog/rest/EntTemplateController.java
@@ -9,9 +9,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,12 +27,50 @@ public class EntTemplateController {
     @Operation(summary = "Get all the templates for the bundle that are in the hub", description = "Public api, no authentication required.")
     @GetMapping("/bundles")
     public List<BundleTemplate> getBundleTemplates() {
-        List<BundleGroupVersion> byTemplateInIt = bundleGroupVersionRepository.getByTemplateInIt();
+        List<BundleGroupVersion> bundleGroupVersionList = bundleGroupVersionRepository.getByTemplateInIt();
 
-        List<BundleTemplate> bundleTemplateList = byTemplateInIt.stream()
+        List<BundleTemplate> bundleTemplateList = bundleGroupVersionList.stream()
                 .flatMap(bundleGroupVersion ->
                         bundleGroupVersion.getBundles().stream().map(bundle -> new BundleTemplate(bundleGroupVersion, bundleGroupVersion.getBundleGroup(), bundle))
-                ).collect(Collectors.toList());
+                )
+                .filter(bundleTemplate ->
+                        bundleTemplate.gitSrcRepoAddress != null && bundleTemplate.gitSrcRepoAddress.length() > 0
+                )
+                .collect(Collectors.toList());
+
+        return bundleTemplateList;
+    }
+
+    @Operation(summary = "Get all the bundle groups having templates in them, they can be filtered by name part", description = "Public api, no authentication required.")
+    @GetMapping("/bundlegroups")
+    public List<BundleGroupTemplate> getBundleGroupsWithTemplates(@RequestParam(required = false) String name) {
+        List<BundleGroupVersion> bundleGroupVersionList;
+        if (name != null) {
+            bundleGroupVersionList = bundleGroupVersionRepository.getByTemplateInItFilteredByName("%" + name + "%");
+        } else {
+            bundleGroupVersionList = bundleGroupVersionRepository.getByTemplateInIt();
+        }
+
+        List<BundleGroupTemplate> bundleGroupTemplateList = bundleGroupVersionList.stream()
+                .map(bundleGroupVersion -> new BundleGroupTemplate(bundleGroupVersion, bundleGroupVersion.getBundleGroup())).collect(Collectors.toList());
+
+        return bundleGroupTemplateList;
+    }
+
+    @Operation(summary = "Get the templates for the bundle given the bundlegroup id", description = "Public api, no authentication required.")
+    @GetMapping("/bundlegroups/{id}")
+    public List<BundleTemplate> getBundleTemplateByBundleGroupId(@PathVariable Long id) {
+        List<BundleGroupVersion> bundleGroupVersionList;
+        bundleGroupVersionList = bundleGroupVersionRepository.getByTemplateInItAndId(id);
+
+        List<BundleTemplate> bundleTemplateList = bundleGroupVersionList.stream()
+                .flatMap(bundleGroupVersion ->
+                        bundleGroupVersion.getBundles().stream().map(bundle -> new BundleTemplate(bundleGroupVersion, bundleGroupVersion.getBundleGroup(), bundle))
+                )
+                .filter(bundleTemplate ->
+                        bundleTemplate.gitSrcRepoAddress != null && bundleTemplate.gitSrcRepoAddress.length() > 0
+                )
+                .collect(Collectors.toList());
 
         return bundleTemplateList;
     }
@@ -58,5 +94,17 @@ public class EntTemplateController {
             this.bundleId = bundle.getId();
         }
     }
+
+    @Data
+    public static class BundleGroupTemplate { //this is a bundle group version containing templates
+        private String bundleGroupName;
+        private Long bundleGroupVersionId;
+
+        public BundleGroupTemplate(BundleGroupVersion bundleGroupVersion, BundleGroup bundleGroup) {
+            this.bundleGroupName = bundleGroup.getName();
+            this.bundleGroupVersionId = bundleGroupVersion.getId();
+        }
+    }
+
 
 }

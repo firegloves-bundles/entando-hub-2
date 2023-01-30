7c310a32f6d60bf809c92671529cf1dbdc15f42c
firegloves <lucorset@gmail.com>
2023-01-30 14:24:22 +0100

    EHUB-263 script to populate/replace bundle ids improvement

--- content/platform/fragments/eh-header-inclusion.ftl
+++ content/platform/fragments/eh-header-inclusion.ftl
@@ -1,4 +1,4 @@
 <#assign wp=JspTaglibs["/aps-core"]>
 
-<link rel="stylesheet" href="<@wp.resourceURL />entando-hub/css/entando-hub.css" media="screen" />
+<link rel="stylesheet" href="<@wp.resourceURL />bundles/entando-hub-content-__CONTENT_BUNDLE_ID__/resources/css/entando-hub.css" media="screen" />
 <link rel="stylesheet" href="<@wp.resourceURL />static/css/all.css" media="screen" />
--- content/set-bundle-id.sh
+++ content/set-bundle-id.sh
@@ -10,37 +10,42 @@
 # by default it searches only in the platform folder, but you can provide a custom location using the -f option
 # by default it searches for ++BUNDLE_ID++ as value to override, but you can provide a custom value using the -t option
 # minimum example usage ./set-bundle-id.sh -u docker://registry.hub.docker.com/alepintus/Dentando-hub-application"
-# full example usage ./set-bundle-id.sh -u docker://registry.hub.docker.com/alepintus/Dentando-hub-application -f microfrontends -t 1111cccc
+# full example usage ./set-bundle-id.sh -u docker://registry.hub.docker.com/alepintus/entando-hub-application -f microfrontends -t 1111cccc
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
     "-f") FOLDER_OVERRIDE="$2";shift;;
-    "-t") TARGET_OVERRIDE="$2";shift;;
-    "-u") BUNDLE_URL="$2";shift;;
+    "-a") APPLICATION_TARGET_OVERRIDE="$2";shift;;
+    "-c") CONTENT_TARGET_OVERRIDE="$2";shift;;
+    "-o") DOCKER_ORG="$2";shift;;
     "-"*) echo "Undefined argument \"$1\"" 1>&2;exit 3;;
   esac
   shift
 done
 
 
-if [ -z "$BUNDLE_URL" ]
+if [ -z "$DOCKER_ORG" ]
 then
-  echo "Please specify a bundle url with -u flag"
+  echo "Please specify a docker org with -o flag"
   exit 1
 fi
 
 FOLDER="${FOLDER_OVERRIDE:-platform}"
-TARGET="${TARGET_OVERRIDE:-++BUNDLE_ID++}"
-NEW_VALUE=$(ent ecr get-bundle-id "${BUNDLE_URL}")
+APPLICATION_TARGET="${APPLICATION_TARGET_OVERRIDE:-__APPLICATION_BUNDLE_ID__}"
+CONTENT_TARGET="${CONTENT_TARGET_OVERRIDE:-__CONTENT_BUNDLE_ID__}"
+APPLICATION_BUNDLE_ID=$(ent ecr get-bundle-id "docker://registry.hub.docker.com/${DOCKER_ORG}/entando-hub-application")
+CONTENT_BUNDLE_ID=$(ent ecr get-bundle-id "docker://registry.hub.docker.com/${DOCKER_ORG}/entando-hub-content")
 
 echo "#########################"
 echo "Folder: ${FOLDER}"
-echo "Url: ${BUNDLE_URL}"
-echo "Value to override: ${TARGET}"
-echo "New value: ${NEW_VALUE}"
+echo "Application placeholder to override: ${APPLICATION_TARGET}"
+echo "Application Bundle ID: ${APPLICATION_BUNDLE_ID}"
+echo "Content placeholder to override: ${CONTENT_TARGET}"
+echo "Content Bundle ID: ${CONTENT_BUNDLE_ID}"
 echo "#########################"
 echo "Replacing..."
 
-grep -rl "${TARGET}" "${FOLDER}" | xargs sed -i "" -e "s/${TARGET}/${NEW_VALUE}/g"
+grep -rl "${APPLICATION_TARGET}" "${FOLDER}" | xargs sed -i "" -e "s/${APPLICATION_TARGET}/${APPLICATION_BUNDLE_ID}/g"
+grep -rl "${CONTENT_TARGET}" "${FOLDER}" | xargs sed -i "" -e "s/${CONTENT_TARGET}/${CONTENT_BUNDLE_ID}/g"
 
 echo "...DONE"

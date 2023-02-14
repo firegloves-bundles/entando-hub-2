126c9ac05623bd7c490bf68ebff323be347c6de1
antromeo <45900043+antromeo@users.noreply.github.com>
2023-02-14 17:55:27 +0100

    EHUB-270: adds integration test for Catalog APIs

--- application/microservices/entando-hub-catalog-ms/src/main/java/com/entando/hub/catalog/service/dto/CatalogDTO.java
+++ application/microservices/entando-hub-catalog-ms/src/main/java/com/entando/hub/catalog/service/dto/CatalogDTO.java
@@ -2,12 +2,14 @@ package com.entando.hub.catalog.service.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class CatalogDTO {
     protected Long id;
     protected Long organisationId;

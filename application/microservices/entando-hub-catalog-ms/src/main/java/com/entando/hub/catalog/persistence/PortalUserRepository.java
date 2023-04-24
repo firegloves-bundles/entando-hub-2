92983dd8abcfffeb13bfcd1a46d7d55f51c9ec21
antromeo <45900043+antromeo@users.noreply.github.com>
2023-04-24 15:22:50 +0200

    EHUB-328: removes unused method signatures from jpaRepository interface

--- application/microservices/entando-hub-catalog-ms/src/main/java/com/entando/hub/catalog/persistence/PortalUserRepository.java
+++ application/microservices/entando-hub-catalog-ms/src/main/java/com/entando/hub/catalog/persistence/PortalUserRepository.java
@@ -1,15 +1,10 @@
 package com.entando.hub.catalog.persistence;
 
-import com.entando.hub.catalog.persistence.entity.Organisation;
 import com.entando.hub.catalog.persistence.entity.PortalUser;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PortalUserRepository extends JpaRepository<PortalUser, Long> {
-    
-    List<PortalUser> findByOrganisationsIs(Organisation organisation);
-
     PortalUser findByUsername(String username);
     
 }
